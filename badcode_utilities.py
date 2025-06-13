3d811061fb442a3b40e33c043e24c5c878d38bc7
cosmo-the-deer <cosmothedeer@gmail.com>
2025-06-14 00:11:56 +1200

    idk what i did

--- badcode_utilities.py
+++ badcode_utilities.py
@@ -25,6 +25,7 @@ info = """
           
 """
 
+characters_letters = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split()
 characters_standard = "a b c d e f g h i j k l m n o p q r s t u v w x y z 1 2 3 4 5 6 7 8 9 0".split()
 characters_advanced = "a b c d e f g h i j k l m n o p q r s t u v w x y z 1 2 3 4 5 6 7 8 9 0 ` ~ ! @ # $ % ^ & * ( ) - _ = + [ ] { } \\ | : ; \" \' , < . > / ?".split()
 characters_numbers = "1 2 3 4 5 6 7 8 9 0".split()
