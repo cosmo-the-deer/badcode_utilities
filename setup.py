46b4c8bff38e51d43341d5b2d87d525f93dd72f4
cosmo-the-deer <cosmothedeer@gmail.com>
2025-06-14 01:17:58 +1200

    build 1.1.2

--- setup.py
+++ setup.py
@@ -2,9 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='badcode_utilities',
-    version='0.1',
+    version='0.1.2',  # or higher, must be unique and not used before
     packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'badcode_utilities': ['badwords.txt'],
+    },
     install_requires=[
-        'random'
+        # none
     ]
 )
